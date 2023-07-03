10bfc63716b6a7c080c13c45ec12c7430e6ec824
arthursvpb <arthurandroid15@gmail.com>
2023-07-03 11:17:50 -0300

    fix float numbers

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -8,7 +8,7 @@ export const calculateBills = (users, expenses) => {
 
   expenses.forEach(expense => {
     const numPayers = expense.payers.length;
-    const splitAmount = expense.price / numPayers;
+    const splitAmount = Math.floor(expense.price / numPayers) * 100;
 
     expense.payers.forEach(payerId => {
       const payer = users.find(user => user.id === payerId);
